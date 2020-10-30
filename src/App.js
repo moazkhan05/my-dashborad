53a2b90972cf3c8e0141134259f7fdd3d5c657f1
moazkhan05 <48621157+moazkhan05@users.noreply.github.com>
2020-10-30 23:24:47 +0500

    FusionChart Example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React , {Component} from 'react';
 import './App.css';
-import Greet from './Components/Greet';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+import Chart from './Components/Chart';
+import Greet from './Components/Greet';
 import Bootstrap from './Components/Bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Greet />
+          <Chart />
           <Bootstrap />
       </div>
     )
